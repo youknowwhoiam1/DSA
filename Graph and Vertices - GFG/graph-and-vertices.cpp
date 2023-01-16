13b71c596ebc8f7ce8af9d520d832f4abb3a3d59
youknowwhoiam1 <95484552+youknowwhoiam1@users.noreply.github.com>
2023-01-16 23:37:09 +0530

    Added solution - LeetHub

---
+++ Graph and Vertices - GFG/graph-and-vertices.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    long long count(int n) {
+        // your code here
+        int cnt = n*(n-1)/2;
+        long long ans = 1;
+        for(int i = 0; i < cnt; i++){
+            ans *= 2;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.count(n) << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
