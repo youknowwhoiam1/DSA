84bd54377e79583ddea4e0bb610127f2620dd3c2
youknowwhoiam1 <95484552+youknowwhoiam1@users.noreply.github.com>
2023-06-04 02:09:55 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,6 +3,36 @@
 using namespace std;
 
 // } Driver Code Ends
+
+const int N = 1005;
+
+vector<int> par(N);
+vector<int> size_of_node(N);
+
+void makeset(int n) {
+    for (int i = 0; i < n; i++) {
+        par[i] = i;
+        size_of_node[i] = 1;
+    }
+}
+
+int get_par(int v) {
+	if (v == par[v])
+		return v;
+	return par[v] = get_par(par[v]);
+}
+
+void union_set(int a, int b) {
+    a = get_par(a);
+    b = get_par(b);
+    if (a != b) {
+        if (size_of_node[a] < size_of_node[b])
+            swap(a, b);
+        par[b] = a;
+        size_of_node[a] += size_of_node[b];
+    }
+}
+
 class Solution
 {
 	public:
@@ -10,28 +40,25 @@ class Solution
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
-        priority_queue<pair<int,int>,
-        vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        vector<int> vis(V,0);
-        pq.push({0,0});
-        int sum = 0;
-        while(!pq.empty()){
-            auto p = pq.top();
-            pq.pop();
-            int node = p.second;
-            int wt = p.first;
-            if(vis[node] == 1){continue;}
-            vis[node] = 1;
-            sum += wt;
-            for(auto it:adj[node]){
-                int nxtnode = it[0];
-                int nxtwt = it[1];
-                if(vis[nxtnode] == 0){
-                    pq.push({nxtwt,nxtnode});
-                }
-            }
-        }
-        return sum;
+        makeset(V);
+    	set<pair<int, pair<int, int>>> v;
+    	for (int i = 0; i < V; i++) {
+    		for (auto j : adj[i]) {
+    			v.insert({j[1], {min(i, j[0]), max(i, j[0])}});
+    		}
+    	}
+    	int sum = 0;
+    	for (auto it = v.begin(); it != v.end(); it++) {
+    		int x = (*it).second.first;
+    		int y = (*it).second.second;
+    
+    		if (get_par(x) != get_par(y)) {
+    			union_set(x, y);
+    			sum += (*it).first;
+    		}
+    	}
+    
+    	return sum;
     }
 };
 
